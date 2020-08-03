0c9a6d7f1acc83ed2660c6566c0361ae319b2009
Alex <alexnewzniyo@gmail.com>
2020-08-03 20:06:20 +0300

    more on contact us

---
+++ app/Http/Controllers/MailSend.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Mail\SendMail;
+class MailSend extends Controller
+{
+    public function mailsend(Request $request){
+        $name = $request['name'];
+        $email = $request['email'];
+        $phone =$request['phone'];
+        $message = $request['message'];
+        $details =[
+            'title' => 'Message From Customer '.$name,
+            'name' => $name,
+            'message'=> $message,
+            'email' => $email,
+            'phone'=> $phone,
+        ];
+        \Mail::to('alexnewzniyo@gmail.com')->send(new SendMail($details));
+        return view('thanks');
+    }
+}
